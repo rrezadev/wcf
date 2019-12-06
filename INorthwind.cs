8a835d73da903ee30b939137a74b9bc760cc1227
Ruben Reza <58441871+rrezadev@users.noreply.github.com>
2019-12-05 17:25:50 -0800

    Create INorthwind.cs

---
+++ INorthwind.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace RR_WCF_NW_11072019
+{
+
+    [ServiceContract]
+    public interface INorthwind
+    {
+        [OperationContract]
+        Product GetProduct(int id);
+    }
+
+    [DataContract]
+    public class Product
+    {
+        [DataMember]
+        public int ProductID { get; set; }
+        [DataMember]
+        public string ProductName { get; set; }
+        [DataMember]
+        public string QuantityPerUnit { get; set; }
+        [DataMember]
+        public decimal UnitPrice { get; set; }
+        [DataMember]
+        public bool Discontinued { get; set; }
+    }
+
+}
